45a4597af870b6e15c230debb53546343a1e0baf
BirjendraRawat <rbirjendra@yahoo.com>
2023-12-07 16:13:56 +0530

    changes

---
+++ FewThingsToTry/Chapter4/Example2.rb
@@ -0,0 +1,8 @@
+# Bigger, better favorite number. Write a program that asks for a person’s
+# favorite number. Have your program add 1 to the number, and then suggest
+# the result as a bigger and better favorite number. (Please be tactful about
+# it, though.)
+
+puts "Enter your favorite Number"
+num = gets.to_i
+puts "This is your new bigger and better favorite no. #{num + 1}"
